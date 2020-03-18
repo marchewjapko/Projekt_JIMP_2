16ff0a3c28c7c60f9f0048146f33deeff4d9e0c5
Bartłomiej Kopyść <kopyscb@ee.pw.edu.pl>
2020-03-18 23:06:52 +0100

    Poprawki, by działały inne rozmiary tablic

--- 1.3/pngFile.c
+++ 1.3/pngFile.c
@@ -17,100 +17,122 @@ png_infop info_ptr;
 int number_of_passes;
 png_bytep * row_pointers;
 
-void upscale(int width, int height, int cord[width][height],int nheight, int nwidth, int upCord[nwidth][nheight], int scale)
+void upscale(int height, int width, int cord[height][width],int nheight, int nwidth, int upCord[nheight][nwidth], int scale)
 {
-	int num = scale - 1;
-	for(int i = 0; i < nwidth; i++) // wpisanie w upCord samych zer
-	{
-		for(int  j = 0; j < nheight; j++)
-			upCord[i][j]=0;
-	}
-	for(int i = 0; i < width; i++)
-	{
 
-		for(int j = 0; j < height; j++)
+	//height - liczba wierszy
+	//width - liczba kolumn
+
+	int num = scale - 1;
+
+	for(int i = 0; i < nheight; i++) // wpisanie w upCord samych zer
+	{
+		for(int  j = 0; j < nwidth; j++)
+		{
+			upCord[i][j]=0;
+			printf("j=%d\n",j);
+		}
+	printf("i=%d\n",i);	
+	}
+
+	for(int i = 0; i < height; i++)
+
+		for(int j = 0; j < width; j++)
 		{
 			if(cord[i][j] == 1)
 			{
+				printf("i:%d j:%d\n", i,j);
 				for(int k = 0; k <= num; k++)
 				{
 					int sec=0;
 					for(int l = 0; l <= num; l++)
 					{
+						int sec = 0;
+
 						upCord[(scale*(i+1))-k-1][(scale*(j+1))-l-1] = 1;
+						printf("Done l %d\n", sec++);
 					}
 				}
 			}
 		}
-	}
+
 } 
 
-void write_png_file(int width, int height, char* file_name, int number, int scale) 
-{
-	FILE *fp = fopen(file_name, "wb");
-  	if (!fp)
-    		printf("[write_png_file] File %s could not be opened for writing", file_name);
 
-  	png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	
 
-  	if (!png_ptr)
-    		printf("[write_png_file] png_create_write_struct failed");
+void write_png_file(int height, int width, char* file_name, int number, int scale) {
 
-  	info_ptr = png_create_info_struct(png_ptr);
-  	if (!info_ptr)
-    		printf("[write_png_file] png_create_info_struct failed");
+  FILE *fp = fopen(file_name, "wb");
+  if (!fp)
+    printf("[write_png_file] File %s could not be opened for writing", file_name);
 
-  	if (setjmp(png_jmpbuf(png_ptr)))
-    		printf("[write_png_file] Error during init_io");
+  png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
 
-  	png_init_io(png_ptr, fp);
+  if (!png_ptr)
+    printf("[write_png_file] png_create_write_struct failed");
 
-  	if (setjmp(png_jmpbuf(png_ptr)))
-    		printf("[write_png_file] Error during writing header");
+  info_ptr = png_create_info_struct(png_ptr);
+  if (!info_ptr)
+    printf("[write_png_file] png_create_info_struct failed");
 
-  	png_set_IHDR(png_ptr, info_ptr, width, height, bit_depth, color_type, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+  if (setjmp(png_jmpbuf(png_ptr)))
+    printf("[write_png_file] Error during init_io");
 
-  	png_write_info(png_ptr, info_ptr);
+  png_init_io(png_ptr, fp);
 
-  	if (setjmp(png_jmpbuf(png_ptr)))
-    		printf("[write_png_file] Error during writing bytes");
+  if (setjmp(png_jmpbuf(png_ptr)))
+    printf("[write_png_file] Error during writing header");
 
-  	png_write_image(png_ptr, row_pointers);
+  png_set_IHDR(png_ptr, info_ptr, width, height,
+   bit_depth, color_type, PNG_INTERLACE_NONE,
+   PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
 
-  	if (setjmp(png_jmpbuf(png_ptr)))
-    		printf("[write_png_file] Error during end of write");
+  png_write_info(png_ptr, info_ptr);
 
-  	png_write_end(png_ptr, NULL);
+  if (setjmp(png_jmpbuf(png_ptr)))
+    printf("[write_png_file] Error during writing bytes");
 
-  	for (y=0; y<height; y++)
-    		free(row_pointers[y]);
-  	free(row_pointers);
+  png_write_image(png_ptr, row_pointers);
 
-  	fclose(fp);
+  if (setjmp(png_jmpbuf(png_ptr)))
+    printf("[write_png_file] Error during end of write");
+
+  png_write_end(png_ptr, NULL);
+
+  for (y=0; y<height; y++)
+    free(row_pointers[y]);
+  free(row_pointers);
+
+  fclose(fp);
 }
 
-void process_file(int width, int height, int cord[width][height], int scale) 
-{
-	bit_depth = 8;
-  	int black = 0;
-  	int white = 255;
-  	color_type = PNG_COLOR_TYPE_GRAY;
+void process_file(int height, int width, int cord[height][width], int scale) {
 
-  	number_of_passes = 7;
-  	row_pointers = (png_bytep*) malloc(sizeof(png_bytep) * height);
-  	for (y=0; y<height; y++)
-    		row_pointers[y] = (png_byte*) malloc(sizeof(png_byte) * width);
+//if(scale > 1)
 
-  	for (y=0; y<height; y++) 
-	{
-    		png_byte* row = row_pointers[y];
-    		for (x=0; x<width; x++) 
-		{
-      			if(cord[y][x]==1)
-	      			row[x]=black;
-      			else
-	      			row[x]=white;
-   	 	}
-  	}
+//  height = height*scale;	
+//  width = width*scale;
+
+  bit_depth = 8;
+  int black = 0;
+  int white = 255;
+  color_type = PNG_COLOR_TYPE_GRAY;
+
+  number_of_passes = 7;
+  row_pointers = (png_bytep*) malloc(sizeof(png_bytep) * height);
+  for (y=0; y<height; y++)
+    row_pointers[y] = (png_byte*) malloc(sizeof(png_byte) * width);
+
+  for (y=0; y<height; y++) {
+    png_byte* row = row_pointers[y];
+    for (x=0; x<width; x++) {
+      if(cord[y][x]==1)
+	      row[x]=black;
+      else
+	      row[x]=white;
+
+    }
+  }
 }
 
--- 1.3/pngFile.h
+++ 1.3/pngFile.h
@@ -1,8 +1,9 @@
+
 #include <stdio.h>
 
-void upscale(int width, int height, int cord[width][height], int nwidth, int nheight,int upcord [nwidth][nheight], int scale);
+void upscale(int height, int width, int cord[height][width], int nheight, int nwidth,int upcord [nheight][nwidth], int scale);
 
-void write_png_file(int width, int height, char* file_name, int number, int scale);
+void write_png_file(int height, int width, char* file_name, int number, int scale);
 
-void process_file(int width, int height, int cord[width][height], int scale);
+void process_file(int height, int width, int cord[height][width], int scale);
 
